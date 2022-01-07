359635cfc0f3b38cbc399cb6288c76fec3c950d9
PandaBurglar <zane.bokhari@gmail.com>
2022-01-06 19:58:51 -0500

    add queries for LightBnB

---
+++ 1_queries/05_all_reservations_for_user.sql
@@ -0,0 +1,10 @@
+SELECT reservations.*, properties.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservation.id 
+ORDER BY reservations.start_date
+LIMIT 10;
+
